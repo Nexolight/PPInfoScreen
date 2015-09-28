0faf6b9f27e3b438b621ac42ee1cb58b027806ca
nexolight <snow.dream.ch@gmail.com>
2015-09-28 17:03:14 +0200

    extendet IOHandler and PPTCOntainer

--- src/net/coldbyte/ppinfscr/io/IOHandler.java
+++ src/net/coldbyte/ppinfscr/io/IOHandler.java
@@ -49,4 +49,6 @@ public class IOHandler {
 			return false;
 		}
 	}
+	
+	
 }
--- src/net/coldbyte/ppinfscr/models/PPTContainer.java
+++ src/net/coldbyte/ppinfscr/models/PPTContainer.java
@@ -12,11 +12,29 @@ import java.io.File;
 public class PPTContainer {
 	
 	private File myPPT;
+	private File me;
 	
 	/**
 	 * This represents a powerpoint file container including it's file
 	 */
-	public PPTContainer(){
-		
+	public PPTContainer(File myPPT, File me){
+		this.myPPT = myPPT;
+		this.me = me;
+	}
+
+	public File getMyPPT() {
+		return myPPT;
+	}
+
+	public void setMyPPT(File myPPT) {
+		this.myPPT = myPPT;
+	}
+
+	public File getMe() {
+		return me;
+	}
+
+	public void setMe(File me) {
+		this.me = me;
 	}
 }
